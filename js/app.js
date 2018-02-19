b8002ece33cddf593563cc74d17fb4d1abb6c90e
costaricawebsolutions <info@costaricawebsolutions.com>
2018-02-19 14:17:13 -0800

    Get number of the days of the week

--- js/app.js
+++ js/app.js
@@ -38,3 +38,37 @@ function getDays(month, year) {
   ar[11] = 31 // December  
   return ar[month]
 }
+function getMonthName(month) {
+  // create array to hold name of each month
+  var ar = new Array(12)
+  ar[0] = "January"
+  ar[1] = "February"
+  ar[2] = "March"
+  ar[3] = "April"
+  ar[4] = "May"
+  ar[5] = "June"
+  ar[6] = "July"
+  ar[7] = "August"
+  ar[8] = "September"
+  ar[9] = "October"
+  ar[10] = "November"
+  ar[11] = "December"
+  // return name of specified month (parameter)
+  return ar[month]
+}
+
+function getWeek(day, lastDate, firstDay) {
+  /** */
+  var week = 0;
+  digit = 1;
+  for (var row = 1; row <= Math.ceil((lastDate + firstDay - 1) / 7); ++row) {
+    for (var col = 1; col <= 7; ++col) {
+      digit++;
+      if (digit == day) {
+        return week;
+      }
+    }
+    week++;
+  }
+}
+
